6b7ea7218170726a170d6fa7d6c78a47192981ce
Shravan Sunder <shravan.retail@gmail.com>
2021-10-04 18:32:03 -0400

    Merge branch 'feature/4/app-provider-refactor' into feature/4/context
    # Conflicts:
    #	yarn.lock

--- packages/vite-app-ts/vite.config.ts
+++ packages/vite-app-ts/vite.config.ts
@@ -4,7 +4,7 @@ import macrosPlugin from 'vite-plugin-babel-macros';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path, { resolve } from 'path';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
-import request from 'request/lib/helpers';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [nodePolyfills(), reactRefresh(), macrosPlugin(), tsconfigPaths()],
