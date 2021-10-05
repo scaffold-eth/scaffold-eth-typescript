1ef68483baf4901db84c074f7dbf604dbd0f7f30
Shravan Sunder <shravan.retail@gmail.com>
2021-10-04 20:24:11 -0400

    catch for missing contract deployment

--- packages/vite-app-ts/src/helpers/loadAppContracts.ts
+++ packages/vite-app-ts/src/helpers/loadAppContracts.ts
@@ -2,6 +2,7 @@ import { TDeployedContracts, TExternalContracts } from 'eth-hooks/models';
 import { TContractConfig } from 'eth-hooks';
 
 const contractListPromise = import('../generated/contracts/hardhat_contracts.json');
+
 /**
  * - run yarn compile-ts and yarn deploy-ts to generate hardhhat_contracts.json
  */
@@ -15,8 +16,21 @@ const externalContractsPromise = import('../generated/contracts/external_contrac
  */
 export const loadAppContracts = async (): Promise<TContractConfig> => {
   const config: TContractConfig = {};
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  config.deployedContracts = ((await contractListPromise).default ?? {}) as unknown as TDeployedContracts;
-  config.externalContracts = ((await externalContractsPromise).default ?? {}) as unknown as TExternalContracts;
+
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    config.deployedContracts = ((await contractListPromise).default ?? {}) as unknown as TDeployedContracts;
+  } catch {
+    config.deployedContracts = undefined;
+    console.warn(
+      '😶 No deployed contracts file found: /generated/contracts/hardhat_contracts.json, please run hardhat compile & deploy!'
+    );
+  }
+  try {
+    config.externalContracts = ((await externalContractsPromise).default ?? {}) as unknown as TExternalContracts;
+  } catch {
+    config.externalContracts = undefined;
+    console.warn('😶 No external contracts file found: /generated/contracts/external_contracts');
+  }
   return config;
 };
