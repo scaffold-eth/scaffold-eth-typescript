b7efb411f87748f41c46b424e7cf5899227a4f2c
Shravan Sunder <shravan.retail@gmail.com>
2021-10-07 03:38:45 -0400

    #4 changed debounce

--- packages/vite-app-ts/src/components/common/FaucetHintButton.tsx
+++ packages/vite-app-ts/src/components/common/FaucetHintButton.tsx
@@ -36,7 +36,7 @@ export const FaucetHintButton: FC<IFaucetButton> = (props) => {
   /**
    * facuet is only available on localhost
    */
-  const faucetAvailable = useDebounce(getFaucetAvailable(props.scaffoldAppProviders, ethersContext), 350, {
+  const faucetAvailable = useDebounce(getFaucetAvailable(props.scaffoldAppProviders, ethersContext), 500, {
     trailing: true,
   });
   const [faucetClicked, setFaucetClicked] = useState(false);
