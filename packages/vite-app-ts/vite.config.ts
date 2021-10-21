42f24a0fbb516db6adc68859fd10dbe3ae3c361d
Shravan Sunder <shravan.sunder.dev@gmail.com>
2021-10-21 00:53:21 -0400

    #21 removed contract-types for now

--- packages/vite-app-ts/vite.config.ts
+++ packages/vite-app-ts/vite.config.ts
@@ -1,23 +1,32 @@
 import { defineConfig } from 'vite';
 //import reactRefresh from '@vitejs/plugin-react-refresh';
 import macrosPlugin from 'vite-plugin-babel-macros';
-import react from '@vitejs/plugin-react';
+import reactPlugin from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path, { resolve } from 'path';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
+import React from 'react';
+import reactdom from 'react-dom';
 
 //console.log('env:dev', process.env.ENVIRONMENT);
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [nodePolyfills(), react(), macrosPlugin(), tsconfigPaths()],
+  plugins: [nodePolyfills(), reactPlugin(), macrosPlugin(), tsconfigPaths()],
 
   build: {
     // sourcemap: true,
     commonjsOptions: {
       include: /node_modules/,
       transformMixedEsModules: true,
-      // esmExternals: true,
+      ...{
+        namedExports: {
+          react: Object.keys(React),
+          'react-dom': Object.keys(reactdom),
+          // '@apollo/client': ['ApolloProvider', 'ApolloClient', 'HttpLink', 'InMemoryCache', 'useQuery', 'gql'],
+          // 'styled-components': ['styled', 'css', 'ThemeProvider'],
+        },
+      },
     },
   },
   esbuild: {
