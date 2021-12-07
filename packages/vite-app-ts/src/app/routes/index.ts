5f418e3a0fe60a100dce4bb02abeb8347659d395
MatteoB <matteobrandolino@gmail.com>
2021-12-07 21:17:28 +0100

    remix-eth-typescript

--- packages/vite-app-ts/src/app/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { lazier } from 'eth-hooks/helpers';
-
-// the components and pages are lazy loaded for performance and bundle size reasons
-// code is in the component file
-
-export const ExampleUI = lazier(() => import('./exampleui/ExampleUI'), 'ExampleUI');
-export const Checkout = lazier(() => import('./checkout/Checkout'), 'Checkout');
-export const Subgraph = lazier(() => import('./subgraph/Subgraph'), 'Subgraph');
-export const Hints = lazier(() => import('./hints/Hints'), 'Hints');
