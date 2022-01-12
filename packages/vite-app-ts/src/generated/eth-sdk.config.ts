f2a8b93612f6c7a528cfc32905b1d68ad46d5249
Shravan Sunder <ShravanSunder@users.noreply.github.com>
2022-01-12 10:31:26 -0500

    updating branches

--- packages/vite-app-ts/src/generated/eth-sdk.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@dethcrypto/eth-sdk';
-import { contractsByNetworkName } from '../functions/contractsByNetworkName';
-
-console.log(contractsByNetworkName);
-
-export default defineConfig({
-  contracts: contractsByNetworkName,
-  outputPath: './src/generated/external-contracts/',
-});
