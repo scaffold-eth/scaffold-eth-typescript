f02bbe2020aa8852313315c8ff9a8739c184c6a2
Shravan Sunder <ShravanSunder@users.noreply.github.com>
2022-02-21 16:40:25 -0500

    #77 update vite config

--- packages/vite-app-ts/vite.config.ts
+++ packages/vite-app-ts/vite.config.ts
@@ -61,7 +61,7 @@ export default defineConfig({
   define: {},
   optimizeDeps: {
     exclude: excludeDeps,
-    include: ['eth-hooks', 'web3Modal', 'eth-components'],
+    include: ['eth-hooks', 'eth-components'],
   },
   resolve: {
     preserveSymlinks: true,
