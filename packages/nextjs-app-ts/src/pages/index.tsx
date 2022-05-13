2febf9a866c4269c4373989140ba5fcf3fb1932d
Shravan Sunder <ShravanSunder@users.noreply.github.com>
2022-05-12 21:54:31 -0400

    #90 trying to fix eslint error

--- packages/nextjs-app-ts/src/pages/index.tsx
+++ packages/nextjs-app-ts/src/pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
@@ -12,7 +10,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
     </div>
