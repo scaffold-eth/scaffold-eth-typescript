7dda68cfe363f8e6593524f57a4783820e245396
Shravan Sunder <ShravanSunder@users.noreply.github.com>
2022-05-12 21:40:29 -0400

    #90 moved mnemonics and css

--- packages/hardhat-ts/tasks/functions/mnemonic.ts
+++ packages/hardhat-ts/tasks/functions/mnemonic.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 //
 // Select the network you want to deploy to here:
 //
-export const mnemonicPath = './generated/mnemonic.secret';
+export const mnemonicPath = './mnemonics/mnemonic.secret';
 export const getMnemonic = (): string => {
   try {
     return fs.readFileSync(mnemonicPath).toString().trim();
--- packages/nextjs-app-ts/src/pages/_app.tsx
+++ packages/nextjs-app-ts/src/pages/_app.tsx
@@ -1,16 +1,17 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import '~~/styles/globals.css';
 import createCache from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
+import type { AppProps } from 'next/app';
+import { FC } from 'react';
 
 const cache = createCache({ key: 'next' });
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <CacheProvider value={cache}>
       <Component {...pageProps} />
     </CacheProvider>
   );
-}
+};
 
 export default MyApp;
