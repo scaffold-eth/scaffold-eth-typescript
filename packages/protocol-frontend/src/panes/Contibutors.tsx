940dec3ba2408e383c7408a288c7b8c8bc37b1f2
Mick00 <20918417+Mick00@users.noreply.github.com>
2022-06-17 15:54:37 -0400

    add basic contributor view

--- packages/protocol-frontend/src/panes/Contibutors.tsx
+++ packages/protocol-frontend/src/panes/Contibutors.tsx
@@ -1,3 +1,39 @@
+import { useCOBApi } from "../components/COBProvider";
+import React, { useEffect, useState } from "react";
+import { Box, Typography } from "@mui/material";
+
+interface IContributor {
+  registrar: string,
+  address: string,
+  hash: string,
+}
+
 export default function Contributors(){
-  return (<p>Contributors</p>)
+  const cobapi = useCOBApi();
+  const contributorsContract = cobapi.contributors();
+  const [contributors, setContributors] = useState<IContributor[]>([])
+
+  useEffect(() => {
+    const filter = contributorsContract.filters['Registered']();
+    contributorsContract.queryFilter(filter).then(registeredEvents => {
+      const unconfirmedContributors = registeredEvents.map((event) => ({
+        registrar: event.args.registrar,
+        address: event.args.contributor,
+        hash: event.args.contributorHash
+      }))
+      setContributors(unconfirmedContributors);
+    });
+  })
+  return (
+    <>
+      <Typography variant={"h3"} sx={{textAlign:"center"}} gutterBottom>Contributors</Typography>
+      {contributors.length === 0 && (
+        <Box><Typography>No contributors found</Typography></Box>
+      )}
+      {contributors.map((contributor) => (
+        <Box><Typography>{contributor.address} registered by {contributor.registrar}</Typography></Box>
+      ))}
+    </>
+
+  )
 }
