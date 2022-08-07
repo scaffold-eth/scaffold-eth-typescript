eaf7355f72234f4a6aeef60ffc64ddb45081a155
Shravan Sunder <ShravanSunder@users.noreply.github.com>
2022-08-07 09:55:03 -0400

    #93 create config working

--- packages/scripts/src/commands/functions/createConfig.ts
+++ packages/scripts/src/commands/functions/createConfig.ts
@@ -2,28 +2,40 @@ import chalk from 'chalk';
 
 import { set, editor } from '~~/helpers/configManager';
 import { TEthereumToolkits, TNetworkNames } from '~common/models';
-import { scaffoldConfigSchema } from '~~/models/TScaffoldConfig';
+import { scaffoldConfigSchema, TScaffoldConfig } from '~~/models/TScaffoldConfig';
 
-export const createConfig = (ethereumToolkitString: string, targetNetworksString: string) => {
-  const targetNetworkArray = targetNetworksString.split(',');
-  const config = scaffoldConfigSchema.safeParse({
-    ethereumToolkit: ethereumToolkitString,
-    targetNetworks: targetNetworkArray,
-  });
-
-  if (config.success == false) {
-    console.log(chalk.red('❌ Error! Invalid config values!'));
-    config.error.errors.forEach((err) => {
-      console.log(chalk.red('  -  ', err.message, ';'));
-    });
-    console.log('arguments: ', ethereumToolkitString, targetNetworksString);
-    return;
-  }
-
-  set('ethereumToolkit', config.data.ethereumToolkit);
-  set('targetNetworks', config.data.targetNetworks);
+export const createConfig = (config: TScaffoldConfig) => {
+  set('ethereumToolkit', config.ethereumToolkit);
+  set('targetNetworks', config.targetNetworks);
 
   editor.save();
   console.log(chalk.green(`✅ Success! Created scaffold.config.json in packages/common`));
-  console.log(chalk.green('With: '), config.data);
+  console.log(chalk.green('With: '), config);
+};
+
+export const parseCreateConfigArgs = (...args: string[]): Parameters<typeof createConfig> => {
+  console.log();
+  try {
+    const ethereumToolkit: TEthereumToolkits = args[0] as TEthereumToolkits;
+    const targetNetworks: TNetworkNames[] = args[1].split(',').map((x) => x.trim()) as TNetworkNames[];
+
+    const config = scaffoldConfigSchema.safeParse({
+      ethereumToolkit: ethereumToolkit,
+      targetNetworks: targetNetworks,
+    });
+
+    if (config.success == false) {
+      console.log(chalk.red('❌ Error! Invalid config values!'));
+      config.error.errors.forEach((err) => {
+        console.log(chalk.red('  -  ', err.message, ';'));
+      });
+      console.log('arguments: ', ethereumToolkit, targetNetworks);
+      throw '';
+    }
+
+    return [config.data];
+  } catch (err) {
+    console.log(chalk.red('❌ Error! Invalid parameters, could not parse them!'));
+    throw err;
+  }
 };
--- packages/scripts/src/commands/scaffoldCommands.ts
+++ packages/scripts/src/commands/scaffoldCommands.ts
@@ -1,14 +1,16 @@
 import { Command } from 'commander';
 
-import { createConfig } from './functions/createConfig';
+import { createConfig, parseCreateConfigArgs } from './functions/createConfig';
 
 const program = new Command();
 
-program.name('scaffold-eth').description('Scaffold-eth-typescript Commands');
+program.name('scaffold').description('Scaffold-eth-typescript Commands');
 program
   .command('create-config')
-  .argument('[ethereum toolkit]', ':  Ethereum tooklit to use (`hardhat` or `foundry`) e.g. hardhat', 'hardhat')
-  .argument('[target networks]', ':  An array of networks to target. e.g. loacalhost, mainnet', 'localhost')
-  .action(createConfig);
+  .argument('[ethereum toolkit]', ':  Ethereum tooklit to use: `hardhat` or `foundry` (e.g. "hardhat")', 'hardhat')
+  .argument('[target networks]', ':  An array of networks to target. (e.g. "localhost, mainnet")', 'localhost')
+  .action((...args: string[]) => {
+    createConfig(...parseCreateConfigArgs(...args));
+  });
 
 program.parse();
